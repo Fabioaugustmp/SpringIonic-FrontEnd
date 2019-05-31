a0c95d44047329c4807cc0082fb27c713e7dbc0f
Fabioaugustmp <fabioaugustmp16@gmail.com>
2019-05-31 13:20:01 -0300

    Menu

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController} from 'ionic-angular/components/app/menu-controller';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,11 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public menu: MenuController ) { 
+    
   }
 
+ public login(){
+      this.navCtrl.setRoot('CategoriesPage');
+  }
 }
